a9e3b20c7d4b11f26b4a19b5b9ad513043da2920
MBSoo <77095328+Byeongsoo-Min@users.noreply.github.com>
2024-12-16 01:01:54 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+SELECT ID, NAME, HOST_ID
+FROM PLACES
+WHERE HOST_ID IN (
+SELECT HOST_ID
+    FROM (
+        SELECT HOST_ID
+        FROM PLACES
+        GROUP BY HOST_ID
+        HAVING COUNT(ID) >= 2
+    ) as A
+)
+ORDER BY ID ASC;
