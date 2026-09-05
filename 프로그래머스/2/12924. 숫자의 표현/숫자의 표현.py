627654f80a671b31cbbf67b97bc667c03d6187b4
MBSoo <77095328+Byeongsoo-Min@users.noreply.github.com>
2026-09-05 23:11:35 +0900

    [level 2] Title: 숫자의 표현, Time: 0.14 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,21 +1,9 @@
 def solution(n):
-    count = 0
-    start = 1
-    end = 1
-    current_sum = 1
+    ans = 1
+    for i in range(2, n):
+        if ((n - sum(range(i))) / i) <= 0 :
+            break
+        if ((n - sum(range(i))) / i).is_integer():
+            ans += 1
+    return ans
     
-    while start <= n:
-        if current_sum == n:
-            count += 1
-            current_sum -= start
-            start += 1
-            
-        elif current_sum < n:
-            end += 1
-            current_sum += end
-            
-        else:  # current_sum > n
-            current_sum -= start
-            start += 1
-            
-    return count
