b94b984c9646665d6ead9f1abacb238e47920948
MBSoo <77095328+Byeongsoo-Min@users.noreply.github.com>
2026-09-05 22:17:52 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.97 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,16 +1,15 @@
 def solution(s):
-    answer = []
-    cnt = 0
-    idx = 0
-    while s != "1": 
-        temp = []
-        idx += 1
-        for i in s:
-            if i == '1':
-                temp.append(i)
-            else :
-                cnt += 1
-        s = str(bin(len(temp)))[2:]
-    answer.append(idx)
-    answer.append(cnt)
-    return answer
+    ans = 0
+    times = 0
+    t = [a for a in s]
+    while True :
+        if len(t) == 1 :
+            break
+        ans += len(t)
+        t = [a for a in t if a != '0']
+        ans -= len(t)
+        t = bin(len(t))[2:]
+        times += 1
+    return [times, ans]
+        
+            
